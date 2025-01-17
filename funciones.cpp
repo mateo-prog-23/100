782c5c18bbfbbfc8f89869733c17172e24ccad2b
mateo-prog-23 <mateopisano23m@gmail.com>
2025-01-16 22:44:34 -0300

    Segundo commit

---
+++ funciones.cpp
@@ -0,0 +1,12 @@
+#include "funciones.h"
+#include <iostream>
+void modoUnJugador(){
+
+
+     std::cout << "Bienvenido al modo un jugador." << endl;
+     std::cout << "Ingrese su nombre : ";
+     cin >>
+}
+
+
+
--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include "funciones.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    cout << "bye bye world" << endl;
+    int opcion;
+    cout << "Ingrese un numero segun la opcion que desee." << endl;
+    cout << "1 - Modo 1 jugador " << endl;
+    cin >> opcion;
+    system("cls");
+
+
+    switch(opcion){
+        case 1: modoUnJugador();
+        break;
+    }
     return 0;
 }
